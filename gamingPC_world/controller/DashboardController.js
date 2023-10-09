918883dd3226f976abc49748e46274b42add31de
warnadeshan50 <warnadeshan50@gmail.com>
2023-10-09 22:52:50 +0530

    init single page application

---
+++ gamingPC_world/controller/DashboardController.js
@@ -0,0 +1,41 @@
+$('#dashboard_section').css('display', 'none');
+
+$('#about_nav').on('click', () => {
+    $('#dashboard_section').css('display', 'block');
+    $('#customer_section').css('display', 'none');
+    $('#item_section').css('display', 'none');
+    $('#order_section').css('display', 'none');
+    $('#invoice_section').css('display', 'none');
+});
+
+$('#customer_nav').on('click', () => {
+    $('#dashboard_section').css('display', 'none');
+    $('#customer_section').css('display', 'block');
+    $('#item_section').css('display', 'none');
+    $('#order_section').css('display', 'none');
+    $('#invoice_section').css('display', 'none');
+});
+
+$('#item_nav').on('click', () => {
+    $('#dashboard_section').css('display', 'none');
+    $('#customer_section').css('display', 'none');
+    $('#item_section').css('display', 'block');
+    $('#order_section').css('display', 'none');
+    $('#invoice_section').css('display', 'none');
+});
+
+$('#order_nav').on('click', () => {
+    $('#dashboard_section').css('display', 'none');
+    $('#customer_section').css('display', 'none');
+    $('#item_section').css('display', 'none');
+    $('#order_section').css('display', 'block');
+    $('#invoice_section').css('display', 'none');
+});
+
+$('#invoice_nav').on('click', () => {
+    $('#dashboard_section').css('display', 'none');
+    $('#customer_section').css('display', 'none');
+    $('#item_section').css('display', 'none');
+    $('#order_section').css('display', 'none');
+    $('#invoice_section').css('display', 'block');
+});
